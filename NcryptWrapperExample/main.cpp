a0663715640ef4ef839e0a8c343c606dc3aa8b96
Steffen André Langnes <steffenl.sw@gmail.com>
2016-06-08 02:13:42 +0200

    Cleaned up a bit more.

--- NcryptWrapperExample/main.cpp
+++ NcryptWrapperExample/main.cpp
@@ -8,9 +8,10 @@
 
 int main()
 {
+    auto provider = KeyStorageProvider::OpenDefault();
+
     // No key name (don't persist key)
     {
-        auto provider = KeyStorageProvider::OpenDefault();
         auto key = provider.CreateKey(BCRYPT_RSA_ALGORITHM, nullptr);
         key.Finalize();
         auto keyBlob = provider.ExportKey(key, BCRYPT_RSAPUBLIC_BLOB);
@@ -19,7 +20,6 @@ int main()
 
     // UTF-16-encoded key name
     {
-        auto provider = KeyStorageProvider::OpenDefault();
         auto key = provider.CreateKey(BCRYPT_RSA_ALGORITHM, L"TestKey");
         key.Finalize();
         auto keyBlob = provider.ExportKey(key, BCRYPT_RSAPUBLIC_BLOB);
@@ -29,7 +29,6 @@ int main()
 
     // UTF-8-encoded key name
     {
-        auto provider = KeyStorageProvider::OpenDefault();
         auto key = provider.CreateKey(BCRYPT_RSA_ALGORITHM, "TestKey");
         key.Finalize();
         auto keyBlob = provider.ExportKey(key, BCRYPT_RSAPUBLIC_BLOB);
